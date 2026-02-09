b38c04a93155b9bccb2f8c36d76c1c158234eadd
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2026-02-09 13:40:13 +0300

    feat(files): deleted

--- backend/app/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Business Logic Services
-
-Service layer for business logic and external integrations.
-"""
-
-from .pdf_service import PDFService, PDFServiceError
-
-__all__ = ["PDFService", "PDFServiceError"]
